45bccefc9b18f2f3bc4a46b1b62d08642a76931e
thesidbatra <thesidbatra@gmail.com>
2019-06-14 22:33:26 -0400

    worked on javascript functionality for psychic word guessing game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,39 @@
 var wins = parseInt(document.getElementById("wins").innerHTML);
 var losses = parseInt(document.getElementById("losses").innerHTML);
 var guessesRemaining = parseInt(document.getElementById("guessesRemaining").innerHTML);
-var guessList = document.getElementById("guessList").innerHTML;
+var guessList = [];
 var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var randomLetter = alphabets[Math.floor(Math.random() * 26) + 1];
+var randomLetter = alphabets[Math.floor(Math.random() * 26)];
 console.log(randomLetter);
+
 document.onkeyup = function (event) {
-    if (event.key == randomLetter) {
+    guessList.push(event.key);
+    document.getElementById("guessList").innerHTML = guessList; //update guess list on HTML
+
+    if (event.key == randomLetter && guessesRemaining > 0) {
         wins++;
-        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("wins").innerHTML = wins; //update wins on HTML
+        guessesRemaining = 9;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining; //update guesses remaining on HTML
+        guessList = [];
+        document.getElementById("guessList").innerHTML = guessList; //update fresh guess list on HTML
+        randomLetter = alphabets[Math.floor(Math.random() * 26)];
+        console.log(randomLetter);
+    }
+    else if (event.key != randomLetter && guessesRemaining > 0) {
+        guessesRemaining--;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining; //update lowered remaining guesses on HTML
     }
     else {
-        guessesRemaining--;
-        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-        document.getElementById("guessList").innerHTML = guessList + ", " + event.key;
-        if (guessesRemaining == -1) {
-            losses++;
-            document.getElementById("losses").innerHTML = losses;
-        }
+        losses++;
+        document.getElementById("losses").innerHTML = losses; //update losses on HTML
+        guessesRemaining = 9;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining; //update lowered remaining guesses on HTML
+        guessList = [];
+        document.getElementById("guessList").innerHTML = guessList; //update fresh guess list on HTML
+        randomLetter = alphabets[Math.floor(Math.random() * 26)];
+        console.log(randomLetter);
     }
+
 }
